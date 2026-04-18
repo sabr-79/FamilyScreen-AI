baf368752056d2985c0813ff7a83f5a5e88ac80f
sabr_79 <sabirinm07976@gmail.com>
2026-04-18 11:15:25 -0500

    skeleton code

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List, Optional
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get('/')
+def index():
+    return {"Msg" : "Hello, world!"}
+
+@app.get("/ping")
+def ping():
+    return {"status": "ok"}
